e699e6635d62b6eb4584f68ddbbc7b9aa42f1997
Thomas Janko <thomas78125@gmail.com>
2024-10-04 10:29:52 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Pricing from './components/Pricing'
+
+const App = () => {
+  return (
+    <main className="overflow-hidden flex flex-col justify-center items-center">
+        <Pricing />
+    </main>
+  )
+}
+
+export default App
